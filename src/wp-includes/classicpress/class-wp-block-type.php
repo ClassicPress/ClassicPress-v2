73d9b54ef0cc91c09ae40c4d9ecb52510c9d00f8
mattyrob <mattyrobuk@gmail.com>
2023-05-01 17:35:13 +0100

    Restore function parameters in WP_Block_Type class

--- src/wp-includes/classicpress/class-wp-block-type.php
+++ src/wp-includes/classicpress/class-wp-block-type.php
@@ -16,36 +16,36 @@
  */
 
 class WP_Block_Type {
-	public function __set( ...$args ) {
+	public function __set( $name, $value ) {
 		global $wp_compat;
 		$wp_compat->using_block_function();
 	}
 
-	public function __get( ...$args ) {
-		global $wp_compat;
-		$wp_compat->using_block_function();
-		return false;
-	}
-
-	public function __isset( ...$args ) {
+	public function __get( $name ) {
 		global $wp_compat;
 		$wp_compat->using_block_function();
 		return false;
 	}
 
-	public function __unset( ...$args ) {
+	public function __isset( $name ) {
 		global $wp_compat;
 		$wp_compat->using_block_function();
 		return false;
 	}
 
-	public function __call( ...$args ) {
+	public function __unset( $name ) {
+		global $wp_compat;
+		$wp_compat->using_block_function();
+		return false;
+	}
+
+	public function __call( $name, $arguments ) {
 		global $wp_compat;
 		$wp_compat::using_block_function();
 		return false;
 	}
 
-	public static function __callstatic( ...$args ) {
+	public static function __callstatic( $name, $arguments ) {
 		global $wp_compat;
 		$wp_compat::using_block_function();
 		return false;
