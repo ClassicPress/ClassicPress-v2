40abbc22a8db7b46f984bdb8f93508bdf2766e04
Viktor Nagornyy <viktor@projectarmy.net>
2023-08-08 17:42:40 -0400

    fix: Reverse branding change in default themes

--- src/wp-content/themes/twentyfifteen/inc/back-compat.php
+++ src/wp-content/themes/twentyfifteen/inc/back-compat.php
@@ -34,7 +34,7 @@ add_action( 'after_switch_theme', 'twentyfifteen_switch_theme' );
  * @since Twenty Fifteen 1.0
  */
 function twentyfifteen_upgrade_notice() {
-	$message = sprintf( __( 'Twenty Fifteen requires at least ClassicPress version 4.1. You are running version %s. Please upgrade and try again.', 'twentyfifteen' ), $GLOBALS['wp_version'] );
+	$message = sprintf( __( 'Twenty Fifteen requires at least WordPress version 4.1. You are running version %s. Please upgrade and try again.', 'twentyfifteen' ), $GLOBALS['wp_version'] );
 	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
@@ -45,7 +45,7 @@ function twentyfifteen_upgrade_notice() {
  */
 function twentyfifteen_customize() {
 	wp_die(
-		sprintf( __( 'Twenty Fifteen requires at least ClassicPress version 4.1. You are running version %s. Please upgrade and try again.', 'twentyfifteen' ), $GLOBALS['wp_version'] ),
+		sprintf( __( 'Twenty Fifteen requires at least WordPress version 4.1. You are running version %s. Please upgrade and try again.', 'twentyfifteen' ), $GLOBALS['wp_version'] ),
 		'',
 		array(
 			'back_link' => true,
@@ -61,7 +61,7 @@ add_action( 'load-customize.php', 'twentyfifteen_customize' );
  */
 function twentyfifteen_preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		wp_die( sprintf( __( 'Twenty Fifteen requires at least ClassicPress version 4.1. You are running version %s. Please upgrade and try again.', 'twentyfifteen' ), $GLOBALS['wp_version'] ) );
+		wp_die( sprintf( __( 'Twenty Fifteen requires at least WordPress version 4.1. You are running version %s. Please upgrade and try again.', 'twentyfifteen' ), $GLOBALS['wp_version'] ) );
 	}
 }
 add_action( 'template_redirect', 'twentyfifteen_preview' );
