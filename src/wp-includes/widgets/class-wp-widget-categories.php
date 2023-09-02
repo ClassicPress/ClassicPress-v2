203846e3ff05a2a48c83ba6c57598b16019c6e7d
Rick Beckman <rick.beckman@gmail.com>
2023-09-01 22:35:43 -0400

    Remove XHTML from class-wp-widget-categories.php
    Remove xhtml/html choice to default to HTML5.
    
    Remove closing slashes on self-closing tags.

--- src/wp-includes/widgets/class-wp-widget-categories.php
+++ src/wp-includes/widgets/class-wp-widget-categories.php
@@ -111,17 +111,10 @@ class WP_Widget_Categories extends WP_Widget {
 
 			<?php
 		} else {
-			$format = current_theme_supports( 'html5', 'navigation-widgets' ) ? 'html5' : 'xhtml';
-
-			/** This filter is documented in wp-includes/widgets/class-wp-nav-menu-widget.php */
-			$format = apply_filters( 'navigation_widgets_format', $format );
-
-			if ( 'html5' === $format ) {
-				// The title may be filtered: Strip out HTML and make sure the aria-label is never empty.
-				$title      = trim( strip_tags( $title ) );
-				$aria_label = $title ? $title : $default_title;
-				echo '<nav aria-label="' . esc_attr( $aria_label ) . '">';
-			}
+			// The title may be filtered: Strip out HTML and make sure the aria-label is never empty.
+			$title      = trim( strip_tags( $title ) );
+			$aria_label = $title ? $title : $default_title;
+			echo '<nav aria-label="' . esc_attr( $aria_label ) . '">';
 			?>
 
 			<ul>
@@ -142,9 +135,7 @@ class WP_Widget_Categories extends WP_Widget {
 			</ul>
 
 			<?php
-			if ( 'html5' === $format ) {
-				echo '</nav>';
-			}
+			echo '</nav>';
 		}
 
 		echo $args['after_widget'];
@@ -186,19 +177,19 @@ class WP_Widget_Categories extends WP_Widget {
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>">
 		</p>
 
 		<p>
-			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'dropdown' ); ?>" name="<?php echo $this->get_field_name( 'dropdown' ); ?>"<?php checked( $dropdown ); ?> />
+			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'dropdown' ); ?>" name="<?php echo $this->get_field_name( 'dropdown' ); ?>"<?php checked( $dropdown ); ?>>
 			<label for="<?php echo $this->get_field_id( 'dropdown' ); ?>"><?php _e( 'Display as dropdown' ); ?></label>
-			<br />
+			<br>
 
-			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'count' ); ?>" name="<?php echo $this->get_field_name( 'count' ); ?>"<?php checked( $count ); ?> />
+			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'count' ); ?>" name="<?php echo $this->get_field_name( 'count' ); ?>"<?php checked( $count ); ?>>
 			<label for="<?php echo $this->get_field_id( 'count' ); ?>"><?php _e( 'Show post counts' ); ?></label>
-			<br />
+			<br>
 
-			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'hierarchical' ); ?>" name="<?php echo $this->get_field_name( 'hierarchical' ); ?>"<?php checked( $hierarchical ); ?> />
+			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id( 'hierarchical' ); ?>" name="<?php echo $this->get_field_name( 'hierarchical' ); ?>"<?php checked( $hierarchical ); ?>>
 			<label for="<?php echo $this->get_field_id( 'hierarchical' ); ?>"><?php _e( 'Show hierarchy' ); ?></label>
 		</p>
 		<?php
