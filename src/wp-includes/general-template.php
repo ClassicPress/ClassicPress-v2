4cc038cb7e801d61c8604ee865a066abeea2b2d3
Rick Beckman <rick.beckman@gmail.com>
2023-09-05 16:34:42 -0400

    Fixes a helper function that output doubled HTML5 attributes.

--- src/wp-includes/general-template.php
+++ src/wp-includes/general-template.php
@@ -5121,7 +5121,7 @@ if ( PHP_VERSION_ID < 80100 ) {
  */
 function __checked_selected_helper( $helper, $current, $display, $type ) { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionDoubleUnderscore,PHPCompatibility.FunctionNameRestrictions.ReservedFunctionNames.FunctionDoubleUnderscore
 	if ( (string) $helper === (string) $current ) {
-		$result = " $type='$type'";
+		$result = " $type";
 	} else {
 		$result = '';
 	}
